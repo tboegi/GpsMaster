63540704ce7358a5173a3e2c1e68041752aa44b5
Torsten Bögershausen <tboegi@web.de>
2019-06-30 08:35:47 +0200

    Create GpsMaster_0_63.31.jar instead of GpsMaster_XX.YY.jar

--- GpsMaster/createJAR.sh
+++ GpsMaster/createJAR.sh
@@ -5,5 +5,5 @@ rsync -ar external/ GpsMaster_jar/ &&
 rsync -ar src/* GpsMaster_jar/ --exclude=*.java &&
 rsync -ar META-INF/ GpsMaster_jar/ &&
 ( cd GpsMaster_jar/ &&
-		jar cmf MANIFEST.MF GpsMaster_XX.YY.jar *
+		jar cmf MANIFEST.MF GpsMaster_0_63.31.jar *
 )
--- GpsMaster/setclasss.sh
+++ GpsMaster/setclasss.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-CLASSES=$(ls ./external/*.jar GpsMaster_jar/GpsMaster_XX.YY.jar)
 if test -d GpsMaster_jar; then
-	CLASSES="$CLASSES $(ls GpsMaster_jar/GpsMaster_XX.YY.jar)"
+	CLASSES="$CLASSES $(ls GpsMaster_jar/GpsMaster_0_63.31.jar)"
+else
+	CLASSES=$(ls ./external/*.jar)
 fi
 CLASSES=$(echo $CLASSES | sed -e 's/ /:/g')
 echo JOPTS="-cp $CLASSES"
