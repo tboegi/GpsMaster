8dcd0129be227e42868fc5d3f29458c25a9ad1dd
Torsten Bögershausen <tboegi@web.de>
2019-08-01 14:58:23 +0200

    createJAR.sh: Generate a .jar file with the right version number (not XX.YY)

--- GpsMaster/createJAR.sh
+++ GpsMaster/createJAR.sh
@@ -20,7 +20,13 @@ case $JVER in
   exit 1
   ;;
 esac
+VERSION_STRING="public static final String VERSION_NUMBER ="
+
+VERSION_NUMBER=$(git grep "$VERSION_STRING" | sed -e "s/^.*://g" -e "s/$VERSION_STRING//g")
+VERSION_NUMBER=$(echo $VERSION_NUMBER | sed -e "s/[^.0-9]//g" -e "s/ //g")
+echo VERSION_NUMBER=$VERSION_NUMBER
 rsync -ar META-INF/ GpsMaster_jar/ &&
 ( cd GpsMaster_jar/ &&
-    jar cmf MANIFEST.MF GpsMaster_XX.YY.$JDKVER.jar *
+    jar cmf MANIFEST.MF GpsMaster_$VERSION_NUMBER.$JDKVER.jar *
 )
+ls -l GpsMaster_jar/GpsMaster_$VERSION_NUMBER.$JDKVER.jar
