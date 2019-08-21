fc48e1b7aee28f8983cf39cd0e5fdac2b6fc1594
Torsten Bögershausen <tboegi@web.de>
2019-08-21 08:12:34 +0200

    Correct setclass.sh: GpsMaster_*.jar
    The final jar file changed it's name -
    use wildcard for "incremental make"

--- GpsMaster/setclasss.sh
+++ GpsMaster/setclasss.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-CLASSES=$(ls ./external/*.jar GpsMaster_jar/GpsMaster_XX.YY.jar)
+CLASSES=$(ls ./external/*.jar)
 if test -d GpsMaster_jar; then
-	CLASSES="$CLASSES $(ls GpsMaster_jar/GpsMaster_XX.YY.jar)"
+	CLASSES="$CLASSES $(ls GpsMaster_jar/GpsMaster_*.jar)"
 fi
 CLASSES=$(echo $CLASSES | sed -e 's/ /:/g')
 echo JOPTS="-cp $CLASSES"
