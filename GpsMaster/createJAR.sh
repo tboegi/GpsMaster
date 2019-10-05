7afab8d24c0423b27ceb4cc17369267a4b838613
Torsten Bögershausen <tboegi@web.de>
2019-10-05 14:55:05 +0200

    createJAR.sh: Add version strings like 1.2.3-rc0

--- GpsMaster/createJAR.sh
+++ GpsMaster/createJAR.sh
@@ -19,8 +19,10 @@ case $JVER in
 esac
 VERSION_STRING="public static final String VERSION_NUMBER ="
 
-VERSION_NUMBER=$(git grep "$VERSION_STRING" | sed -e "s/^.*://g" -e "s/$VERSION_STRING//g")
-VERSION_NUMBER=$(echo $VERSION_NUMBER | sed -e "s/[^.0-9]//g" -e "s/ //g")
+# Extract the VERSION string from java source code, remove everything until the first digit
+# giving a result like this '0.63.33-rc0";' after the first sed
+# Remove the '"' abd ';' in the second sed expression
+VERSION_NUMBER=$(git grep "$VERSION_STRING" "*.java" | sed -e "s/^.*VERSION_NUMBER =[^0-9]*//g" -e s/[^-.0-9a-zA-Z]//g)
 echo VERSION_NUMBER=$VERSION_NUMBER
 rsync -ar META-INF/ GpsMaster_jar/ &&
 ( cd GpsMaster_jar/ &&
