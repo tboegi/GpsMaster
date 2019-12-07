04666fc2ecd68298366a0145ce48cba50a7e402f
Torsten Bögershausen <tboegi@web.de>
2019-12-07 12:18:55 +0100

    Step up version to 0.63.34

--- GpsMaster/src/org/gpsmaster/GpsMaster.java
+++ GpsMaster/src/org/gpsmaster/GpsMaster.java
@@ -197,7 +197,7 @@ import eu.fuegenstein.util.LogEntry;
 public class GpsMaster extends JComponent {
 
 	public static final String PROGRAM_NAME = "GpsMaster";
-	public static final String VERSION_NUMBER = "0.63.34_b1";
+	public static final String VERSION_NUMBER = "0.63.34";
 	public static final String ME = PROGRAM_NAME + " " + VERSION_NUMBER;
 
     // indents show layout hierarchy
