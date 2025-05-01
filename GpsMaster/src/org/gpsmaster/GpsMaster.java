2e02cb0647fa02bbab69a201ac2bbe8fd4e49c47
Torsten Bögershausen <tboegi@web.de>
2025-05-01 11:31:33 +0200

    Prepare 0.64.05

--- GpsMaster/src/org/gpsmaster/GpsMaster.java
+++ GpsMaster/src/org/gpsmaster/GpsMaster.java
@@ -197,7 +197,7 @@ import eu.fuegenstein.util.LogEntry;
 public class GpsMaster extends JComponent {
 
     public static final String PROGRAM_NAME = "GpsMaster";
-    public static final String VERSION_NUMBER = "0.64.04";
+    public static final String VERSION_NUMBER = "0.64.05";
     public static final String ME = PROGRAM_NAME + " " + VERSION_NUMBER;
 
     // indents show layout hierarchy
